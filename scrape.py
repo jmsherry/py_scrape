e7167515b12b43e513cb0b90f675ba3a00456ae5
James Sherry <james.m.sherry@googlemail.com>
2019-04-03 11:09:53 +0100

    vertical spacing

--- scrape.py
+++ scrape.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import sys
 import re
-from lxml import html
 import requests
 from bs4 import BeautifulSoup
 
@@ -19,13 +18,14 @@ def scrape(args):
     r'\[?[A-F0-9]*:[A-F0-9:]+\]?)' # ...or ipv6
     r'(?::\d+)?' # optional port
     r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-  print('args[1]', args[1])
+  # print('URL', args[1])
   if (not re.match(regex, args[1])):
     raise(f'{args[1]} is not a valid URL')
 
   page = requests.get(args[1])
   soup = BeautifulSoup(page.content, 'html.parser')
 
+  print('\n')
   for el in soup.find_all(args[2]):
     print(el, '\n')
 
